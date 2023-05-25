1f8bef02e3e7d154fe699719efa717e1dee519f1
Hristijan <chrispetrov904@gmail.com>
2023-05-25 13:50:08 +0200

    Changed the js file to use classes instead of factory functions

--- js.js
+++ js.js
@@ -1,34 +1,39 @@
-function Book(name,author) {
-    this.name=name
-    this.author=author
-    this.read=false
-  }
+class Book {
+    constructor(name,author){
+        this.name=name
+        this.author=author
+    }
+    addBookToLibrary(book) {
+        myLibrary.push(book)
+        const newP=document.createElement("p")
+        const buttRemove=document.createElement("button")
+
+        newP.setAttribute("data-number", myLibrary.length-1)
+        buttRemove.innerText='Remove Book'
+        buttRemove.setAttribute("data-number", myLibrary.length-1)
+        buttRemove.addEventListener("click", removeBook)
+        newP.innerHTML= book.name + " " + book.author
+    
+        
+        newP.appendChild(buttRemove)
+        contain.appendChild(newP)
+    }
+}
 
 let myLibrary = [];
 const contain = document.getElementById('container')
+const library= new Book();
 const bookOne = new Book("Teddy", "Rosefalt")
-addBookToLibrary(bookOne);
+console.log(bookOne)
+library.addBookToLibrary(bookOne);
 const bookTwo = new Book("Mad", "House")
-addBookToLibrary(bookTwo);
+library.addBookToLibrary(bookTwo);
 
 
 
 
 
-function addBookToLibrary(book) {
-    myLibrary.push(book)
-    const newP=document.createElement("p")
-    const buttRemove=document.createElement("button")
-    newP.setAttribute("data-number", myLibrary.length-1)
-    buttRemove.innerText='Remove Book'
-    buttRemove.setAttribute("data-number", myLibrary.length-1)
-    buttRemove.addEventListener("click", removeBook)
-    newP.innerHTML= book.name + " " + book.author
-    
-   
-    newP.appendChild(buttRemove)
-    contain.appendChild(newP)
-}
+
 
 function checkLibrary(){
     contain.innerHTML='<button onclick="addBook()">Add new Book</button>'
@@ -59,7 +64,7 @@ function addBook(){
     const name=prompt("Enter the Name of the Book")
     const author=prompt("Enter the author of the Book")
     const newBook= new Book(name,author)
-    addBookToLibrary(newBook)
+    library.addBookToLibrary(newBook)
     console.log(myLibrary)
 }
 
